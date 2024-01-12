cf3b3d046cdab64869758cc75a358d7686667b05
Jurayevkh <jurayevkh@gmail.com>
2024-01-12 23:02:36 +0500

    Promocode model created

---
+++ src/About/About.Domain/Entities/Promocodes/Promocode.cs
@@ -0,0 +1,9 @@
+﻿namespace About.Domain.Entities.Promocodes;
+
+public class Promocode:BaseEntity
+{
+    public string Promokod { get; set; }
+    public float Sum { get; set; }
+    public DateTime ExpiryDate { get; set;}
+}
+
