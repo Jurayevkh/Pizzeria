a19cbad08da773af93e8a866fc57093489660e65
Jurayevkh <jurayevkh@gmail.com>
2024-01-12 23:02:49 +0500

    Abstraction created

---
+++ src/About/About.Application/Abstractions/IApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using About.Domain.Entities.AboutCompany;
+using About.Domain.Entities.Actions;
+using About.Domain.Entities.Branches;
+using About.Domain.Entities.Promocodes;
+using Microsoft.EntityFrameworkCore;
+
+namespace About.Application.Abstractions;
+
+public interface IApplicationDbContext
+{
+    public DbSet<AboutCompany> AboutCompanies { get; set; }
+    public DbSet<Actions> Actions { get; set; }
+    public DbSet<Branch> Branches { get; set; }
+    public DbSet<Promocode> Promocodes { get; set; }
+
+    public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+}
+
