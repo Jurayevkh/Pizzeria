d756d65baa9ea758d44d23c150b889b9e134db14
Jurayevkh <jurayevkh@gmail.com>
2024-01-12 23:01:58 +0500

    AboutCompany Model Created

---
+++ src/About/About.Domain/Entities/AboutCompany/AboutCompany.cs
@@ -0,0 +1,9 @@
+﻿namespace About.Domain.Entities.AboutCompany;
+
+public class AboutCompany:BaseEntity
+{
+    public string Contact { get; set; }
+    public string Email { get; set; }
+    public string Location { get; set; }
+}
+
