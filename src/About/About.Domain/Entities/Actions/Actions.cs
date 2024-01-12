38aa0643f9e326ac2bc453be7b8fbb5c7cdbeb14
Jurayevkh <jurayevkh@gmail.com>
2024-01-12 23:02:07 +0500

    Actoin model created

---
+++ src/About/About.Domain/Entities/Actions/Actions.cs
@@ -0,0 +1,9 @@
+﻿namespace About.Domain.Entities.Actions;
+
+public class Actions:BaseEntity
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string ImagePath { get; set; }
+}
+
