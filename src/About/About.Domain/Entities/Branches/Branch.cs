468b8c8dfeddc7a0b8d1e24cc9a3adc12ccf5a5e
Jurayevkh <jurayevkh@gmail.com>
2024-01-12 23:02:22 +0500

    Branch model created

---
+++ src/About/About.Domain/Entities/Branches/Branch.cs
@@ -0,0 +1,9 @@
+﻿namespace About.Domain.Entities.Branches;
+
+public class Branch : BaseEntity
+{
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string WorkSchedule {get;set;}
+}
+
