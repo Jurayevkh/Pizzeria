012b507ed916287c4c006a1f099dad3ccf725caa
Jurayevkh <jurayevkh@gmail.com>
2024-01-16 21:01:31 +0500

    Db of Order Service

---
+++ src/Order/Order.Infrastructure/Data/OrderDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Order.Application.Abstractions;
+using Order.Domain.Entities.Basket;
+using Order.Domain.Entities.Order;
+
+namespace Order.Infrastructure.Data;
+
+public class OrderDbContext : DbContext, IApplicationDbContext
+{
+    public OrderDbContext(DbContextOptions<OrderDbContext> options):base(options)
+    {
+
+    }
+
+    public DbSet<BasketItems> BasketItems { get; set; }
+    public DbSet<Baskets> Baskets { get; set; }
+    public DbSet<Orders> Orders { get; set; }
+    public DbSet<OrderItems> OrderItems { get; set; }
+
+}
+
