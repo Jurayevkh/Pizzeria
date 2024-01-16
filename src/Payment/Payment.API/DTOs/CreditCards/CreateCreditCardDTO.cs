c92b46c8e77605524cfd08bc957c407765d790e7
Jurayevkh <jurayevkh@gmail.com>
2024-01-17 00:29:26 +0500

    DTOs of Models

---
+++ src/Payment/Payment.API/DTOs/CreditCards/CreateCreditCardDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Payment.API.DTOs.CreditCards;
+
+public class CreateCreditCardDTO
+{
+    public string CustomerPhoneNumber { get; set; }
+    public string CardNumber { get; set; }
+    public string ExpiryDate { get; set; }
+    public string CVV { get; set; }
+}
+
---
+++ src/Payment/Payment.API/DTOs/Payments/CreatePaymentDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Payment.API.DTOs.Payments;
+
+public class CreatePaymentDTO
+{
+    public int OrderId { get; set; }
+    public string PaymentMethod { get; set; }
+    public string Promocode { get; set; }
+}
+
