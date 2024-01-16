c6a206d6370f40e61778e17ac55e6e247620410c
Jurayevkh <jurayevkh@gmail.com>
2024-01-17 00:31:34 +0500

    global usings

---
+++ src/Payment/Payment.Application/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using MediatR;
+global using Payment.Domain.Entities.CreditCard;
+global using Payment.Application.UseCases.CreditCards.Commands;
+global using Payment.Application.Abstractions;
+global using Microsoft.EntityFrameworkCore;
+global using Payment.Application.UseCases.CreditCards.Queries;
+
