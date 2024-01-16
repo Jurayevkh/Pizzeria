6b95a291b43d856bdb10c160a75f811b448ea52c
Jurayevkh <jurayevkh@gmail.com>
2024-01-17 00:32:56 +0500

    DependencyInjection of Infrastructure layer

---
+++ src/Payment/Payment.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Payment.Application.Abstractions;
+using Payment.Infrastructure.Data;
+
+namespace Payment.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<IApplicationDbContext, PaymentDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        return services;
+    }
+}
+
