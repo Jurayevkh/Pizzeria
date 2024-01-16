806248e9dc2c256fad60f18f0a6e83f979a355ea
Jurayevkh <jurayevkh@gmail.com>
2024-01-16 20:50:38 +0500

    Added ApplicationDbContext

---
+++ src/AccountCatalog/AccountCatalog.Application/Abstractions/IApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AccountCatalog.Domain.Entities.Category;
+using AccountCatalog.Domain.Entities.Customer;
+using AccountCatalog.Domain.Entities.Product;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccountCatalog.Application.Abstractions;
+
+public interface IApplicationDbContext
+{
+    public DbSet<Categories> Categories { get; set; }
+    public DbSet<Customers> Customers { get; set; }
+    public DbSet<Products> Products { get; set; }
+
+    public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
+
