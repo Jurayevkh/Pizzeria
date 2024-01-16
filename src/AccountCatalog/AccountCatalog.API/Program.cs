001bc557e41ac0bfa213862d272c217ce90d61e4
Jurayevkh <jurayevkh@gmail.com>
2024-01-16 20:49:44 +0500

    Connection string referenced to infrastructure layer

--- src/AccountCatalog/AccountCatalog.API/Program.cs
+++ src/AccountCatalog/AccountCatalog.API/Program.cs
@@ -1,15 +1,16 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using AccountCatalog.Infrastructure;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
