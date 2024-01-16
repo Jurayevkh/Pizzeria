f955c1e525362d4cb655475d5ea645612bbf4733
Jurayevkh <jurayevkh@gmail.com>
2024-01-16 19:00:54 +0500

    Deleted Action, AboutCompany Table

--- src/About/About.Application/Abstractions/IApplicationDbContext.cs
+++ src/About/About.Application/Abstractions/IApplicationDbContext.cs
@@ -1,6 +1,4 @@
-﻿using About.Domain.Entities.AboutCompany;
-using About.Domain.Entities.Actions;
-using About.Domain.Entities.Branches;
+﻿using About.Domain.Entities.Branches;
 using About.Domain.Entities.Promocodes;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,8 +6,6 @@ namespace About.Application.Abstractions;
 
 public interface IApplicationDbContext
 {
-    public DbSet<AboutCompany> AboutCompanies { get; set; }
-    public DbSet<Actions> Actions { get; set; }
     public DbSet<Branch> Branches { get; set; }
     public DbSet<Promocode> Promocodes { get; set; }
 
