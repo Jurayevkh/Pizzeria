c8f7ba04de788260a49879f07c94201639d902ea
Jurayevkh <jurayevkh@gmail.com>
2024-01-16 20:53:48 +0500

    Db of AccountCatalog service

---
+++ src/AccountCatalog/AccountCatalog.Infrastructure/Data/AccountCatalogDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using AccountCatalog.Application.Abstractions;
+using AccountCatalog.Domain.Entities.Category;
+using AccountCatalog.Domain.Entities.Customer;
+using AccountCatalog.Domain.Entities.Product;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccountCatalog.Infrastructure.Data;
+
+public class AccountCatalogDbContext : DbContext, IApplicationDbContext
+{
+    public AccountCatalogDbContext(DbContextOptions<AccountCatalogDbContext> options):base(options)
+    {
+            
+    }
+
+    public DbSet<Categories> Categories { get; set; }
+    public DbSet<Customers> Customers { get; set; }
+    public DbSet<Products> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetCallingAssembly());
+    }
+}
+
