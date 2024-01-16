b7f9642fa83c88208675852f6d1d812d210b0e5c
Jurayevkh <jurayevkh@gmail.com>
2024-01-16 20:53:27 +0500

    Seed data of Category Model

---
+++ src/AccountCatalog/AccountCatalog.Infrastructure/Configurations/CategoryTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using AccountCatalog.Domain.Entities.Category;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AccountCatalog.Infrastructure.Configurations;
+
+public class CategoryTypeConfiguration : IEntityTypeConfiguration<Categories>
+{
+    public void Configure(EntityTypeBuilder<Categories> builder)
+    {
+        builder.HasData(new Categories { Id=1,Name="Kombolar"},
+            new Categories { Id=2,Name="Pitsalar"},
+            new Categories { Id=3,Name="Ichimliklar"},
+            new Categories { Id=4,Name="Desertlar"});
+    }
+}
+
