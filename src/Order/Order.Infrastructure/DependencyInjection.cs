6a4165f0dc75263fc76f3718a53210eccf8854ed
Jurayevkh <jurayevkh@gmail.com>
2024-01-16 21:03:18 +0500

    DependencyInjection of Infrastructure layer

---
+++ src/Order/Order.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Order.Application.Abstractions;
+using Order.Infrastructure.Data;
+
+namespace Order.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<IApplicationDbContext, OrderDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        return services;
+    }
+}
+
