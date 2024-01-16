ee56da1544970d4b68ae471d31bbddb2f688e50c
Jurayevkh <jurayevkh@gmail.com>
2024-01-17 00:32:11 +0500

    Db of Payment service

---
+++ src/Payment/Payment.Infrastructure/Data/PaymentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Payment.Application.Abstractions;
+using Payment.Domain.Entities.CreditCard;
+using Payment.Domain.Entities.Payment;
+
+namespace Payment.Infrastructure.Data;
+
+public class PaymentDbContext : DbContext, IApplicationDbContext
+{
+    public PaymentDbContext(DbContextOptions<PaymentDbContext> options):base(options)
+    {
+
+    }
+
+    public DbSet<CreditCards> CreditCards { get; set; }
+    public DbSet<Payments> Payments { get; set; }
+}
+
