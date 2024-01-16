e28cb718857078c64325db231c9261b4eb209902
Jurayevkh <jurayevkh@gmail.com>
2024-01-16 20:57:11 +0500

    Db and Fluent APis of AccountCatalog

---
+++ src/AccountCatalog/AccountCatalog.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using AccountCatalog.Application.Abstractions;
+using AccountCatalog.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AccountCatalog.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<IApplicationDbContext, AccountCatalogDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        return services;
+    }
+}
+
