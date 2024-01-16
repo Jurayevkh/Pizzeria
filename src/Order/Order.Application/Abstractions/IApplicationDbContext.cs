edfc51eddb3b4796e79bcd959dc6994d70f72f71
Jurayevkh <jurayevkh@gmail.com>
2024-01-16 21:00:25 +0500

    Added Abstractions

---
+++ src/Order/Order.Application/Abstractions/IApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Order.Domain.Entities.Basket;
+using Order.Domain.Entities.Order;
+
+namespace Order.Application.Abstractions;
+
+public interface IApplicationDbContext
+{
+    public DbSet<BasketItems> BasketItems { get; set; }
+    public DbSet<Baskets> Baskets { get; set; }
+    public DbSet<Orders> Orders { get; set; }
+    public DbSet<OrderItems> OrderItems { get; set; }
+
+    public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
+
