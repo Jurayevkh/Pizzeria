07fd0fe897c76542c9fa49a4d9ebc7ee39edf311
Jurayevkh <jurayevkh@gmail.com>
2024-01-17 00:30:20 +0500

    IApplicationDbContext

---
+++ src/Payment/Payment.Application/Abstractions/IApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Payment.Domain.Entities.CreditCard;
+using Payment.Domain.Entities.Payment;
+
+namespace Payment.Application.Abstractions;
+
+public interface IApplicationDbContext
+{
+    public DbSet<CreditCards> CreditCards { get; set; }
+    public DbSet<Payments> Payments { get; set; }
+
+    public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+}
+
