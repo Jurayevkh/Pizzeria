05ed87dbee294c7dc0dd07c977fa1ff89b3164a8
Jurayevkh <jurayevkh@gmail.com>
2024-01-18 06:07:06 +0500

    DependencyInjection of Application layer

---
+++ src/About/About.Application/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿namespace About.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+        return services;
+    }
+}
+
