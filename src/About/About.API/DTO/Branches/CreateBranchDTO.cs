9dd4359f3ac3df5bf132cb1fb1a82231ec34496e
Jurayevkh <jurayevkh@gmail.com>
2024-01-18 06:04:38 +0500

    Branch DTOs

---
+++ src/About/About.API/DTO/Branches/CreateBranchDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace About.API.DTO.Branches;
+
+public class CreateBranchDTO
+{
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string WorkSchedule { get; set; }
+
+}
+
---
+++ src/About/About.API/DTO/Branches/UpdateBranchDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace About.API.DTO.Branches;
+
+public class UpdateBranchDTO
+{
+    public string? Name { get; set; }
+    public string? Location { get; set; }
+    public string? WorkSchedule { get; set; }
+}
+
