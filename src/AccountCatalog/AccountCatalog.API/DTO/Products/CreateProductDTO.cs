e3317f71d5af13ea9b3abb3d3ce327faafc3e074
Jurayevkh <jurayevkh@gmail.com>
2024-01-18 06:09:26 +0500

    Product DTOs

---
+++ src/AccountCatalog/AccountCatalog.API/DTO/Products/CreateProductDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace AccountCatalog.API.DTO.Products;
+
+public class CreateProductDTO
+{
+    public string Name { get; set; }
+    public float Price { get; set; }
+    public string Recipe { get; set; }
+    public string ImagePath { get; set; }
+    public string CategoryName { get; set; }
+}
+
---
+++ src/AccountCatalog/AccountCatalog.API/DTO/Products/UpdateProductDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace AccountCatalog.API.DTO.Products;
+
+public class UpdateProductDTO
+{
+    public string OldName { get; set; }
+    public string? Name { get; set; }
+    public float? Price { get; set; }
+    public string? Recipe { get; set; }
+    public string? ImagePath { get; set; }
+    public string? CategoryName { get; set; }
+}
+
