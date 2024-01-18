5d9dc0267a76e6e5b41e976fa517e9140fd9fcc0
Jurayevkh <jurayevkh@gmail.com>
2024-01-18 06:13:02 +0500

    Global Usings

---
+++ src/AccountCatalog/AccountCatalog.API/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using Microsoft.AspNetCore.Mvc;
+global using MediatR;
+global using AccountCatalog.Application.UseCases.Categories.Queries;
+global using AccountCatalog.Application.UseCases.Categories.Commands;
