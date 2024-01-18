eb14485e49a9f82c1fdcaa1227809e4ab19c20b0
Jurayevkh <jurayevkh@gmail.com>
2024-01-18 06:05:31 +0500

    global usings

---
+++ src/About/About.API/GlobalUsings.cs
@@ -0,0 +1,11 @@
+﻿global using About.Application;
+global using About.Infrastructure;
+global using Microsoft.AspNetCore.Mvc;
+global using MediatR;
+global using About.Application.UseCases.Branches.Queries;
+global using About.API.DTO.Branches;
+global using About.Application.UseCases.Branches.Commands;
+global using About.Application.UseCases.Promocodes.Queries;
+global using About.API.DTO.Promocodes;
+
+
