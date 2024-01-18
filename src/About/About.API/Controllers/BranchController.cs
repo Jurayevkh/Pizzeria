e605cbee38ee4b0f68bc585208097f704a59f06e
Jurayevkh <jurayevkh@gmail.com>
2024-01-18 06:04:18 +0500

    Controllers About Service

---
+++ src/About/About.API/Controllers/BranchController.cs
@@ -0,0 +1,61 @@
+﻿namespace About.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class BranchController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BranchController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllBranch()
+        {
+            var branch = await _mediator.Send(new GetAllBranchesQuery());
+            return Ok(branch);
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetBranchByName(string Name)
+        {
+            var branch = await _mediator.Send(new GetByNameBranchQuery{ Name = Name });
+            return Ok(branch);
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateBranch(CreateBranchDTO createBranchDTO)
+        {
+            CreateBranchCommand branch = new CreateBranchCommand()
+            {
+                Name = createBranchDTO.Name,
+                Location = createBranchDTO.Location,
+                WorkSchedule = createBranchDTO.WorkSchedule
+            };
+            var result = await _mediator.Send(branch);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async ValueTask<IActionResult> UpdateBranch(UpdateBranchDTO updateBranchDTO)
+        {
+            UpdateBranchCommand branchCommand = new UpdateBranchCommand()
+            {
+                Name=updateBranchDTO.Name,
+                Location=updateBranchDTO.Location,
+                WorkSchedule=updateBranchDTO.WorkSchedule
+            };
+            var result = await _mediator.Send(branchCommand);
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteBranch(string Name)
+        {
+            var result = await _mediator.Send(new DeleteBranchCommand() { Name=Name});
+            return Ok(result);
+        }
+    }
+}
+
---
+++ src/About/About.API/Controllers/PromocodeController.cs
@@ -0,0 +1,78 @@
+﻿
+using About.Application.UseCases.Promocodes.Commands;
+
+namespace About.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class PromocodeController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public PromocodeController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllPromocode()
+        {
+            var promocodes = await _mediator.Send(new GetAllPromocodeQuery());
+            return Ok(promocodes);
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetPromocodeByPromocode(string Promocode)
+        {
+            var promocode = await _mediator.Send(new GetPromocodeByPromocodeQuery { Promocode = Promocode });
+            return Ok(promocode);
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> CheckPromocode(string Promcode)
+        {
+            var promocode = await _mediator.Send(new CheckExpiryOfPromocodeCommand { Promocode=Promcode});
+            return Ok(promocode);
+        }
+
+
+        [HttpPost]
+        public async ValueTask<IActionResult> CreatePromocode(CreatePromocodeDTO createPromocodeDTO)
+        {
+            if (createPromocodeDTO.Days < 0 || createPromocodeDTO.SumOfDiscount < 0)
+                return Ok(false);
+
+            CreatePromocodeCommand promocode = new CreatePromocodeCommand()
+            {
+                Promocode = createPromocodeDTO.Promocode,
+                SumOfDiscount = createPromocodeDTO.SumOfDiscount,
+                Days = createPromocodeDTO.Days
+            };
+            var result = await _mediator.Send(promocode);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async ValueTask<IActionResult> UpdatePromocode(UpdatePromocodeDTO updatePromocodeDTO)
+        {
+            if (updatePromocodeDTO.Days < 0 || updatePromocodeDTO.SumOfDisscount < 0)
+                return Ok(false);
+            UpdatePromocodeCommand promocode = new UpdatePromocodeCommand()
+            {
+               OldPromocode = updatePromocodeDTO.OldPromocode,
+               Promocode=updatePromocodeDTO.Promocode,
+               SumOfDisscount=updatePromocodeDTO.SumOfDisscount,
+               Days=updatePromocodeDTO.Days
+            };
+            var result = await _mediator.Send(promocode);
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeletePromocode(string Promocode)
+        {
+            var result = await _mediator.Send(new DeletePromocodeCommand() { Promocode = Promocode });
+            return Ok(result);
+        }
+    }
+}
+
