7561dae784844bbc90f9e5caeedd0bd97a7a4443
Jurayevkh <jurayevkh@gmail.com>
2024-01-18 06:05:00 +0500

    DTOs of Promocode

---
+++ src/About/About.API/DTO/Promocodes/CreatePromocodeDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace About.API.DTO.Promocodes;
+
+public class CreatePromocodeDTO
+{
+    public string Promocode { get; set; }
+    public float SumOfDiscount { get; set; }
+    public int Days { get; set; }
+}
+
---
+++ src/About/About.API/DTO/Promocodes/UpdatePromocodeDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace About.API.DTO.Promocodes;
+
+public class UpdatePromocodeDTO
+{
+    public string OldPromocode { get; set; }
+    public string? Promocode { get; set; }
+    public float? SumOfDisscount { get; set; }
+    public int? Days { get; set; }
+}
+
