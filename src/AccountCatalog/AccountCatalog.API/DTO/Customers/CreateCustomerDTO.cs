db9c1d0432eba99a02ee27f388c6988f12f7c005
Jurayevkh <jurayevkh@gmail.com>
2024-01-18 06:08:47 +0500

    Customer DTOs

---
+++ src/AccountCatalog/AccountCatalog.API/DTO/Customers/CreateCustomerDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace AccountCatalog.API.DTO.Customers;
+
+public class CreateCustomerDTO
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Password { get; set; }
+}
+
---
+++ src/AccountCatalog/AccountCatalog.API/DTO/Customers/UpdateCustomerDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace AccountCatalog.API.DTO.Customers;
+
+public class UpdateCustomerDTO
+{
+    public string PhoneNumber { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? PhoneNumberChange { get; set; }
+    public string? Password { get; set; }
+}
+
