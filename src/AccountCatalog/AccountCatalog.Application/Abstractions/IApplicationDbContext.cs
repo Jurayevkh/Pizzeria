c8e1f55a164e901392d9af2721ff917a41eaaa1e
Jurayevkh <jurayevkh@gmail.com>
2024-01-20 23:20:55 +0500

    removed useless namespaces

--- src/AccountCatalog/AccountCatalog.Application/Abstractions/IApplicationDbContext.cs
+++ src/AccountCatalog/AccountCatalog.Application/Abstractions/IApplicationDbContext.cs
@@ -1,15 +1,11 @@
-﻿using AccountCatalog.Domain.Entities.Category;
-using AccountCatalog.Domain.Entities.Customer;
-using AccountCatalog.Domain.Entities.Product;
-using Microsoft.EntityFrameworkCore;
-
-namespace AccountCatalog.Application.Abstractions;
+﻿namespace AccountCatalog.Application.Abstractions;
 
 public interface IApplicationDbContext
 {
     public DbSet<Categories> Categories { get; set; }
     public DbSet<Customers> Customers { get; set; }
     public DbSet<Products> Products { get; set; }
+    public DbSet<Roles> Roles { get; set; }
 
     public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
