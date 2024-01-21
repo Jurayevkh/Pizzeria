5e0cc3cbf5900bd402a47d852738f101fcfd24a8
Jurayevkh <jurayevkh@gmail.com>
2024-01-21 22:55:30 +0500

    Change model

--- src/Payment/Payment.Domain/Entities/Payment/Payments.cs
+++ src/Payment/Payment.Domain/Entities/Payment/Payments.cs
@@ -4,8 +4,7 @@ public class Payments:BaseEntity
 {
     public int OrderId { get; set; }
     public DateTime PaymentDate { get; set; } = DateTime.Now;
-    public string PaymentMethod { get; set;}
-    public int? CardId { get; set; }
+    public int CardId { get; set; }
     public int? PromoCodeId { get; set; }
 
 }
