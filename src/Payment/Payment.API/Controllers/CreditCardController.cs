a4f779849796742eecdef3f83bdc23c42ee0909c
Jurayevkh <jurayevkh@gmail.com>
2024-01-21 22:54:52 +0500

    Controllers

---
+++ src/Payment/Payment.API/Controllers/CreditCardController.cs
@@ -0,0 +1,45 @@
+﻿namespace Payment.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class CreditCardController : Controller
+    {
+
+        private readonly IMediator _mediator;
+
+        public CreditCardController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllCreditCard()
+        {
+            var creditCards = await _mediator.Send(new GetAllCreditCardQuery());
+            return Ok(creditCards);
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetCreditCard(string CardNumber)
+        {
+            var creditCard = await _mediator.Send(new GetCreditCardByCardNumberQuery() {CardNumber=CardNumber});
+            return Ok(creditCard);
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateCreditCard(CreateCreditCardDTO createCreditCardDTO)
+        {
+            CreateCreditCardCommand creditCards = new CreateCreditCardCommand()
+            {
+                CustomerPhoneNumber=createCreditCardDTO.CustomerPhoneNumber,
+                CardNumber=createCreditCardDTO.CardNumber,
+                ExpiryDate=createCreditCardDTO.ExpiryDate,
+                CVV=createCreditCardDTO.CVV
+            };
+
+            var creditCard = await _mediator.Send(creditCards);
+            return Ok(creditCard);
+        }
+
+    }
+}
+
---
+++ src/Payment/Payment.API/Controllers/PaymentController.cs
@@ -0,0 +1,42 @@
+﻿using Payment.Application.UseCases.Payments.Queries;
+
+namespace Payment.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class PaymentController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public PaymentController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> CreatePayment(CreatePaymentDTO createPaymentDTO)
+        {
+            CreatePaymentCommand payment = new CreatePaymentCommand()
+            {
+                OrderId=createPaymentDTO.OrderId,
+                PromoCode=createPaymentDTO.Promocode,
+                CardId=createPaymentDTO.CardId
+            };
+            var payments = await _mediator.Send(payment);
+            return Ok(payments);
+        }
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllPayments()
+        {
+            var payments = await _mediator.Send(new GetAllPaymentsQuery());
+            return Ok(payments);
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetPaymentsByOrderId(int OrderId)
+        {
+            var payments = await _mediator.Send(new GetAllPaymentsByOrderIdQuery() { OrderId=OrderId});
+            return Ok(payments);
+        }
+    }
+}
+
