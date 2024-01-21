394b18024f88e1ce37d4da3e829fb3644b17ef33
Jurayevkh <jurayevkh@gmail.com>
2024-01-21 22:54:42 +0500

    Migrations

---
+++ src/Order/Order.Infrastructure/Migrations/20240120215733_changeInBasket.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Order.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeInBasket : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderedAt",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Baskets");
+
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "BasketItems");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "OrderedAt",
+                table: "Orders",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Baskets",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "BasketItems",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
